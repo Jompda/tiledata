16f6054af567ac8fd82a116df08ca1de4709335a
Jompda <45610052+Jompda@users.noreply.github.com>
2022-10-28 18:07:15 +0300

    Changed the example a bit

--- example/index.js
+++ example/index.js
@@ -10,6 +10,7 @@ setConfig({
         {
             name: 'elevation',
             type: 'wmts',
+            // Source: https://docs.mapbox.com/data/tilesets/reference/mapbox-terrain-dem-v1/
             url: `https://api.mapbox.com/v4/mapbox.mapbox-terrain-dem-v1/{z}/{x}/{y}.pngraw?access_token=${options.mapboxToken}`,
             valueFunction: function (R, G, B) {
                 return -10000 + (R * 256 * 256 + G * 256 + B) * 0.1;
@@ -18,10 +19,27 @@ setConfig({
         {
             name: 'treeHeight',
             type: 'wms',
+            // Attribution: © Luonnonvarakeskus, 2019, keskipituus_1519, Monilähteisen valtakunnan metsien inventoinnin (MVMI) kartta-aineisto 2017
             url: 'https://kartta.luke.fi/geoserver/MVMI/ows?',
             layers: 'keskipituus_1519',
             valueFunction: function (r, g, b) {
-                return Math.ceil(rgbToTreeHeight([r, g, b]))
+                // Colors retrieved from: https://kartta.luke.fi/geoserver/MVMI/ows?service=WMS&version=1.3.0&request=GetLegendGraphic&format=image/png&width=20&height=20&layer=keskipituus_1519
+                // Due to value incrementation and the last increment being 220dm - infinity, let's just use Finland's tallest tree as the max value :D
+                const values = new Map([
+                    ['255,255,255', 0],
+                    ['151,71,73', 0],
+                    ['254,114,0', 1.3],
+                    ['254,152,70', 5.7],
+                    ['254,205,165', 8.5],
+                    ['195,255,195', 10.7],
+                    ['131,243,115', 12.5],
+                    ['24,231,22', 14.3],
+                    ['2,205,0', 16.1],
+                    ['1,130,0', 18.4],
+                    ['23,0,220', 21.9],
+                    ['40,31,149', 47]
+                ])
+                return Math.ceil(values.get([r, g, b].join(',')))
             }
         }
     ],
@@ -30,36 +48,6 @@ setConfig({
     },
     getDataByTile: (name) => tileDataStorage.get(name)
 })
-
-
-/**
- * Hyödynnetävä aineisto:
- * © Luonnonvarakeskus, 2019, keskipituus_1519, Monilähteisen valtakunnan metsien inventoinnin (MVMI) kartta-aineisto 2017
- * värit haettu osoitteesta:
- * https://kartta.luke.fi/geoserver/MVMI/ows?service=WMS&version=1.3.0&request=GetLegendGraphic&format=image/png&width=20&height=20&layer=keskipituus_1519
- * Luken tiedostopalvelusta saa ladattua lehtijaon mukaan laatat, joissa puiden korkeus on 16x16m sluilla desimetrin tarkkuudella.
- * Arvoja ei olla porrastettu samoin kuin WMS-palvelimella ja aineisto tarjoaa myös korkeampia arvoja käytöön kuin 220dm.
- * Koska arvot ovat porrastettu ja viimeinen väri kattaa 220dm - ääretön, niin käytetään sitten suomen korkeimman puun pituutta kyseisellä arvolla :D
- * @param {*} rgbArray 
- * @returns 
- */
-function rgbToTreeHeight(rgbArray) {
-    const values = new Map([
-        ['255,255,255', 0],
-        ['151,71,73', 0],
-        ['254,114,0', 1.3],
-        ['254,152,70', 5.7],
-        ['254,205,165', 8.5],
-        ['195,255,195', 10.7],
-        ['131,243,115', 12.5],
-        ['24,231,22', 14.3],
-        ['2,205,0', 16.1],
-        ['1,130,0', 18.4],
-        ['23,0,220', 21.9],
-        ['40,31,149', 47]
-    ])
-    return values.get(rgbArray.join(','))
-}
 
 
 //const projectedLatLng = proj4('EPSG:3857', 'EPSG:4326').forward([2808285, 9608542])
